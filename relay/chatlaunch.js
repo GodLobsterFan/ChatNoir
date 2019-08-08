13031811b20e879bff5dfa93c5862a66fcca3ba2
GodLobsterFan <53130563+GodLobsterFan@users.noreply.github.com>
2019-08-08 18:53:14 +0800

    Update chatlaunch.js

--- relay/chatlaunch.js
+++ relay/chatlaunch.js
@@ -1,5 +1,18 @@
+var snowmen = ["Flakey","Freezey","Frosty","Snowflake","Snowy"];
+
 function clean(x)
 {
+	# Disable snowmen
+	if (snowmen.includes(x["who"]["name"]))
+	{
+		return;
+	}
+	# Disable pirate yelling
+	x["msg"] = x["msg"].replace("<b>", "").replace("</b>", "");
+	# Disable safari noises
+	x["msg"] = x["msg"].replace(/<i title="(.*?)">.*?<\/i>/gi, "$1");
+	# Force chat colours to be black
 	x["msg"] = x["msg"].replace(/font color=\w*>/gi, 'font color=black>').replace(/color: #\w+/gi, "color: #000000").replace(/font-weight: \w+/gi, "font-weight: normal");
+
 	handleMessage(x);
 }
