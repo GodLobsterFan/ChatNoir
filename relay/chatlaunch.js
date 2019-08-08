b67c127c082a94793b95874efdbb0ca9666278d9
GodLobsterFan <53130563+GodLobsterFan@users.noreply.github.com>
2019-08-08 22:30:21 +0800

    Update chatlaunch.js
    Maybe I should learn Javascript one of these days.

--- relay/chatlaunch.js
+++ relay/chatlaunch.js
@@ -2,16 +2,16 @@ var snowmen = ["Flakey","Freezey","Frosty","Snowflake","Snowy"];
 
 function clean(x)
 {
-	# Disable snowmen
+	// Disable snowmen
 	if (snowmen.includes(x["who"]["name"]))
 	{
 		return;
 	}
-	# Disable pirate yelling
+	// Disable pirate yelling
 	x["msg"] = x["msg"].replace("<b>", "").replace("</b>", "");
-	# Disable safari noises
+	// Disable safari noises
 	x["msg"] = x["msg"].replace(/<i title="(.*?)">.*?<\/i>/gi, "$1");
-	# Force chat colours to be black
+	// Force chat colours to be black
 	x["msg"] = x["msg"].replace(/font color=\w*>/gi, 'font color=black>').replace(/color: #\w+/gi, "color: #000000").replace(/font-weight: \w+/gi, "font-weight: normal");
 
 	handleMessage(x);
