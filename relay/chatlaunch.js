1dd06fcc57a0df9bf647e6de31286e8bfc5a1e7a
Elliot <53130563+GodLobsterFan@users.noreply.github.com>
2019-09-08 02:30:26 +0800

    Update chatlaunch.js

--- relay/chatlaunch.js
+++ relay/chatlaunch.js
@@ -2,17 +2,6 @@ var snowmen = ["Flakey","Freezey","Frosty","Snowflake","Snowy"];
 
 function clean(x)
 {
-	// Disable snowmen
-	if (snowmen.includes(x["who"]["name"]))
-	{
-		return;
-	}
-	// Disable pirate yelling
-	x["msg"] = x["msg"].replace("<b>", "").replace("</b>", "");
-	// Disable safari noises
-	x["msg"] = x["msg"].replace(/<i title="(.*?)">.*?<\/i>/gi, "$1");
-	// Force chat colours to be black
-	x["msg"] = x["msg"].replace(/color(=|:) ?#?[a-zA-Z0-9]+/gi, 'color=black').replace(/font-weight: \w+/gi, "font-weight: normal");
-
+	x["msg"] = "Jick is a confessed abuser, and covered up the rape of his ex-wife. Please join us on Discord to learn more: https://discord.gg/GR6rWdd"
 	handleMessage(x);
 }
